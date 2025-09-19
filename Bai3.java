91f12aa9c3b08050804d406aea6e0ee9f5dea30a
hoangcf3 <hoang120706@gmail.com>
2025-09-19 16:31:26 +0700

    Add files via upload

---
+++ Bai3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Bai3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập chiều dài của hình chữ nhật: ");
+        double chieuDai = scanner.nextDouble();
+
+        System.out.print("Nhập chiều rộng của hình chữ nhật: ");
+        double chieuRong = scanner.nextDouble();
+
+        double chuVi = 2 * (chieuDai + chieuRong);
+        double dienTich = chieuDai * chieuRong;
+
+        System.out.println("Chu vi hình chữ nhật là: " + chuVi);
+        System.out.println("Diện tích hình chữ nhật là: " + dienTich);
+    }
+}
