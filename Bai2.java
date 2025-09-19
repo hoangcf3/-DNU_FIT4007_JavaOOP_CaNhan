e8e20de8d5b14becdadd3e80ccdf3a41e2796d18
hoangcf3 <hoang120706@gmail.com>
2025-09-19 16:25:59 +0700

    Add files via upload

---
+++ Bai2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+    public class Bai2 {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Nhập bán kính hình tròn: ");
+            double r = scanner.nextDouble();
+
+            double chuVi = 2 * Math.PI * r;
+            double dienTich = Math.PI * r * r;
+
+            System.out.println("Chu vi hình tròn là: " + chuVi);
+            System.out.println("Diện tích hình tròn là: " + dienTich);
+        }
+    }
